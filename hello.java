b50b634c37dc9e4d4ce5399e0399dd87866cd9f1
sujitharajendran <sujikavi66@gmail.com>
2017-06-07 09:18:33 +0530

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,13 @@
+package hello;
+
+public class hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method 
+	int r =5;
+	for(int i=0;i<r;i++)
+		{System.out.println("hello" );
+		}
+
+}
+}
